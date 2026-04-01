37c53e2afce1d2f6c358fe046c12d2cb5d308fda
FyreByrns <horace5413@gmail.com>
2026-04-01 09:09:12 -0700

    before trip

--- TileBased/Tilemap.cs
+++ TileBased/Tilemap.cs
@@ -39,16 +39,17 @@ struct Tilemap {
 }
 
 class TileQuadTree {
-/*
-    Quadtree that subdivides to the individual tile level.
-    Collapses like leaves into larger leaves if possible.
+    /*
+        Quadtree that subdivides to the individual tile level.
+        Collapses like leaves into larger leaves if possible.
 
-    Worst-case: checkerboard of unlike tiles
-    Best-case: large plane of like tiles
-*/
+        Worst-case: checkerboard of unlike tiles
+        Best-case: large plane of like tiles
+    */
 
     public Tile Tile = Tile.NONE;
     public bool IsBaseLeaf => TL == null || TR == null || BL == null || BR == null;
+    public bool HoldsBaseLeaves => !IsBaseLeaf && (TL.IsBaseLeaf && TR.IsBaseLeaf && BL.IsBaseLeaf && BR.IsBaseLeaf);
     TileQuadTree? TL, TR, BL, BR;
 
     public IntegerAABB Bounds;
@@ -79,21 +80,72 @@ class TileQuadTree {
     }
 
     public bool SetTile(TilemapCoordinate coordinate, Tile tile) {
-        if(Bounds.ContainsPoint(coordinate.X, coordinate.Y)) {
-            if(IsBaseLeaf) {
+        if (Bounds.ContainsPoint(coordinate.X, coordinate.Y)) {
+            if (IsBaseLeaf) {
                 // if this leaf is the base leaf and the tile matches the current tile
-                if (Tile == tile) { return true; }
+                if (Tile == tile) {
+                    return true;
+                } // nothing needs to happen
 
+                // if this leaf is the base leaf and its size is 1
+                if (Bounds.Width == 1 && Bounds.Height == 1) {
+                    Tile = tile;
+                    return true;
+                } // set this tile to the input tile
 
+                // otherwise, need to split
+                TL = new(new(Bounds.Left, Bounds.Top, Bounds.MidX, Bounds.MidY));
+                TR = new(new(Bounds.MidX, Bounds.Top, Bounds.Right, Bounds.MidY));
+                BL = new(new(Bounds.Left, Bounds.MidY, Bounds.MidX, Bounds.Bottom));
+                BR = new(new(Bounds.MidX, Bounds.MidY, Bounds.Right, Bounds.Bottom));
             }
+
+            // try to add to children
+            if (TL!.SetTile(coordinate, tile)) { return true; }
+            if (TR!.SetTile(coordinate, tile)) { return true; }
+            if (BL!.SetTile(coordinate, tile)) { return true; }
+            if (BR!.SetTile(coordinate, tile)) { return true; }
+
+            // all failed, so the tile has not been set
+            return false;
         }
 
         return false;
     }
+
+    /// <summary>
+    /// Attempt to collapse like children into a larger node.
+    /// Returns whether or not a collapse occurred. 
+    /// </summary>
+    public bool Collapse() {
+        if (IsBaseLeaf) { return false; }
+
+        if(HoldsBaseLeaves) {
+            if (TL.IsBaseLeaf && TR.IsBaseLeaf && BL.IsBaseLeaf && BR.IsBaseLeaf) {
+
+            }
+        }
+
+        while (TL.Collapse() || TR.Collapse() || BL.Collapse() || BR.Collapse()) { 
+            // loop while collapses may happen.
+        }
+
+        return false;
+    }
+
+    public TileQuadTree(IntegerAABB bounds) {
+        Bounds = bounds;
+    }
 }
 
-struct IntegerAABB(int top, int left, int right, int bottom) {
+struct IntegerAABB(int left, int top, int right, int bottom) {
     public int Top = top, Left = left, Right = right, Bottom = bottom;
+
+    public int Width => Right - Left;
+    public int Height => Bottom - Top;
+
+    public int MidX => Left + Width / 2;
+    public int MidY => Top + Height / 2;
 
     public bool ContainsPoint(int x, int y) {
         return !(Top >= y && Left >= x && Right <= x && Bottom <= y);
